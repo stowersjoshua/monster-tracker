b12c8f53d13999b1d2267795d88581af03774dc8
stowersjoshua <stowers.joshua@live.com>
2020-08-08 18:21:58 -0500

    Add basic Monster class

--- encounter.rb
+++ encounter.rb
@@ -1,12 +1,24 @@
 require 'json'
+require_relative 'monster'
 
 class Encounter
-  attr_accessor :name
+  attr_accessor :name, :monsters
 
-  def initialize(encounter_file_location)
-    encounter_file = File.read(encounter_file_location)
+  def initialize(source_file)
+    encounter_file = File.read(source_file)
     encounter_json = JSON.parse(encounter_file)
 
     @name = encounter_json["name"]
+    @monsters = []
+
+    encounter_json["monsters"].each do |monster_data|
+      name, race, max_health = monster_data.values_at("name", "race", "max_health")
+      monster = Monster.new(name: name, race: race, max_health: max_health, source_file: source_file)
+      @monsters.append(monster)
+    end
+  end
+
+  def monster_names
+    monsters.map { |monster| monster.name }
   end
 end
--- track_health.rb
+++ track_health.rb
@@ -4,3 +4,4 @@ encounter_file_location = 'data/encounters/encounter-1.json'
 encounter = Encounter.new(encounter_file_location)
 
 puts "Encounter Name: #{encounter.name}"
+puts "Monster Names: #{encounter.monster_names.join(', ')}"
