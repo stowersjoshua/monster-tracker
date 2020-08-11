efa87e40c907c82a22c4382881c51683eb2d60a5
stowersjoshua <stowers.joshua@live.com>
2020-08-10 21:53:18 -0500

    Re-request encounter name if filename would be invalid
    Encounter name validation logic can be expanded later to check for filename conflicts.
    
    The ActiveSupport gem has been added to give us access to it's [string helpers](https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/string.rb).
    I can't live without the .present? and .blank? methods.
    Ruby has .empty?, but it isn't defined on NilClass, so it isn't as safe to use.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.0'
 
-
+gem 'activesupport'
--- manage_encounter.rb
+++ manage_encounter.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/string'
 require 'json'
 
 SAFE_FILENAME_CHARACTERS = /[\w-]/.freeze
 ENCOUNTERS_DIRECTORY = 'data/encounters'
 
-def sanitize_filename(filename, extension:)
-  filename = filename.downcase.gsub(' ', '-')
-  sanitized_filename = filename.scan(SAFE_FILENAME_CHARACTERS).join
-  "#{sanitized_filename}.#{extension}"
+def sanitize_filename(base_name, extension: nil)
+  base_name = base_name.downcase.gsub(' ', '-')
+  sanitized_filename = base_name.scan(SAFE_FILENAME_CHARACTERS).join
+
+  return unless sanitized_filename.present?
+  sanitized_filename << ".#{extension}" if extension.present?
+  puts ''
+
+  sanitized_filename
 end
 
-# Ask for encounter name
-puts 'Encounter Name:'
-encounter_name = gets.chomp
+def encounter_name_valid? encounter_name
+  sanitized_name = sanitize_filename(encounter_name)
+  sanitized_name.present?
+end
 
-# Assemble encounter data
+def request_encounter_name(message)
+  puts message
+  encounter_name = gets.chomp
+
+  if encounter_name_valid? encounter_name
+    encounter_name
+  else
+    request_encounter_name('Invalid encounter name. Please try again.')
+  end
+end
+
+# Collect encounter data
+encounter_name = request_encounter_name('Encounter Name:')
 encounter_data = { name: encounter_name }
 
 # Create JSON file
