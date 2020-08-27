c07907cc949890d596c22e78b2f1fed2aa52bc3c
stowersjoshua <stowers.joshua@live.com>
2020-08-26 19:18:45 -0500

    Abstract Encounter file loader to dedicated method
    This is the first step towards making the file optional so that we can load from keyword arguments instead when desired.

--- encounter.rb
+++ encounter.rb
@@ -1,25 +1,35 @@
 require_relative 'monster'
 
+require 'active_support/core_ext/string'
 require 'json'
 
 class Encounter
-  attr_accessor :name, :monsters
+  attr_accessor :name, :monsters, :encounter_file
 
   def initialize(encounter_file)
-    encounter_file = File.read(encounter_file)
-    encounter_json = JSON.parse(encounter_file)
+    self.encounter_file = encounter_file
 
-    @name = encounter_json["name"]
-    @monsters = []
-
-    encounter_json["monsters"].each do |monster_data|
-      name, race, max_health = monster_data.values_at("name", "race", "max_health")
-      monster = Monster.new(name: name, race: race, max_health: max_health, encounter_file: encounter_file)
-      @monsters.append(monster)
-    end
+    load_from_encounter_file
   end
 
   def monster_names
     monsters.map { |monster| monster.name }
+  end
+
+  private
+
+  def load_from_encounter_file
+    return if encounter_file.blank?
+
+    encounter_json = File.read(encounter_file)
+    encounter_data = JSON.parse(encounter_json)
+
+    monsters = encounter_data['monsters'].map do |monster_data|
+      name, race, max_health = monster_data.values_at('name', 'race', 'max_health')
+      Monster.new(name: name, race: race, max_health: max_health, encounter_file: encounter_file)
+    end
+
+    self.name = encounter_data['name']
+    self.monsters = monsters
   end
 end
