a917d51383f126ff24cb478404a4fb2d6cee9469
stowersjoshua <stowers.joshua@live.com>
2020-08-26 19:01:16 -0500

    Refactor source_file to encounter_file

--- encounter.rb
+++ encounter.rb
@@ -1,11 +1,12 @@
-require 'json'
 require_relative 'monster'
+
+require 'json'
 
 class Encounter
   attr_accessor :name, :monsters
 
-  def initialize(source_file)
-    encounter_file = File.read(source_file)
+  def initialize(encounter_file)
+    encounter_file = File.read(encounter_file)
     encounter_json = JSON.parse(encounter_file)
 
     @name = encounter_json["name"]
@@ -13,7 +14,7 @@ class Encounter
 
     encounter_json["monsters"].each do |monster_data|
       name, race, max_health = monster_data.values_at("name", "race", "max_health")
-      monster = Monster.new(name: name, race: race, max_health: max_health, source_file: source_file)
+      monster = Monster.new(name: name, race: race, max_health: max_health, encounter_file: encounter_file)
       @monsters.append(monster)
     end
   end
--- monster.rb
+++ monster.rb
@@ -1,11 +1,11 @@
 class Monster
-  attr_accessor :name, :race, :max_health, :current_health, :source_file
+  attr_accessor :name, :race, :max_health, :current_health, :encounter_file
 
-  def initialize(name:, race:, max_health:, source_file: nil)
+  def initialize(name:, race:, max_health:, encounter_file: nil)
     @name = name
     @race = race
     @max_health = max_health
-    @source_file = source_file
+    @encounter_file = encounter_file
 
     @health = @max_health
   end
